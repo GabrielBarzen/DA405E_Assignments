80afcd4b556d53eba13435fb3e7eea7b85c72db8
Gabriel Modin Bärzén <gabriel.barzen@mailfence.com>
2022-10-03 12:29:03 +0200

    116_Unidirectional_TSP solved, runtime err

--- OnlineJudge/116_Unidirectional_TSP/src/Main.java
+++ OnlineJudge/116_Unidirectional_TSP/src/Main.java
@@ -2,42 +2,120 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
     static int[][] map;
     static int[][] costs;
     public static void main(String[] args) throws IOException {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-        String strIN = "";
-        while ((strIN = in.readLine()) != null){
-            String[] strPair = strIN.split(" ");
-            int y = Integer.parseInt(strPair[0]);
-            int x = Integer.parseInt(strPair[1]);
+
+        String strIn;
+
+        StringBuilder sb = new StringBuilder();
+        while ((strIn=in.readLine())!=null && strIn.length()!=0) {
+            if (strIn.split(" ").length == 2) sb.append("# ");
+            sb.append(strIn);
+            sb.append(" ");
+        }
+        sb.append("|");
+
+        StringTokenizer st = new StringTokenizer(sb.toString());
+        int y;
+        int x;
+        while(true){
+            String startToken = st.nextToken();
+            if (startToken.equals("|")) break;
+
+            y = Integer.parseInt(st.nextToken());
+            x = Integer.parseInt(st.nextToken());
+
             map = new int[y][x];
-            costs = new int[map.length][map[0].length];
-            for (int i = 0; i < y; i++) {
-                String[] currLine = in.readLine().split(" ");
-                for (int i1 = 0; i1 < x; i1++) {
-                    map[i][i1] = Integer.parseInt(currLine[i1]);
+            costs = new int[y][x];
+
+            for (int i = 0; i < map.length; i++) {
+                for (int j = 0; j < map[0].length; j++) {
+
+                    map[i][j] = Integer.parseInt(st.nextToken());
+
                 }
             }
-            printArrs();
-            System.out.println("init arrs");
-            init();
-            System.out.println("init done");
-            printArrs();
-            System.out.println("running all mindist");
 
+
+            init();
             for (int i = 0; i < y; i++) {
-                minDist(i, x-1);
+                if (map[i][x-1] != 0) {
+                    minDist(i, x - 1);
+                }
             }
-            minPath();
-            System.out.println("mindist done");
-            printArrs();
+
+
+            int[] result = minPath();
+
+            for (int i = 0; i < result.length; i++) {
+                if (i==result.length-1) {
+                    System.out.println();
+                    System.out.println(result[i]);
+                    continue;
+                }
+                System.out.print((result[i] + 1) + " ");
+            }
+
         }
     }
 
-    private static void minPath() {
+    private static int[]  minPath() {
+        //Find start path
+        int y = 0;
+        int startPosCost = Integer.MAX_VALUE;
+        for (int i = 1; i < costs.length; i++) {
+            if (costs[i][costs[0].length-1] < costs[y][costs[0].length-1]) {
+                y = i;
+                startPosCost = costs[i][costs[0].length-1];
+                continue;
+            }
+            if (costs[i][costs[0].length-1] < startPosCost) startPosCost = costs[i][costs[0].length-1];
+        }
+
+
+
+        int[] minPath = new int[map[0].length+1];
+        minPath[minPath.length-2] = y;
+        minPath[minPath.length-1] = startPosCost;
+
+
+        for (int x = minPath.length-3; x >= 0; x--) {
+            int smallestCost = costs[y][costs[0].length-1];
+
+            int[] yOpt = new int[3]; // Create array for previous best options
+            yOpt[0] = ((y-1)+map.length)%(map.length);
+            yOpt[1] = y;
+            yOpt[2] = (y+1)%(map.length);
+
+            sort(yOpt);
+
+            for (int i = 0; i < yOpt.length; i++) {
+                if (costs[yOpt[i]][x] < smallestCost) {
+                    y = yOpt[i];
+                    smallestCost = costs[y][x];
+                }
+            }
+            minPath[x] = y;
+        }
+        return minPath;
+    }
+
+    private static void sort(int[] in) {
+        //Basic selection sort
+        for (int i = 0; i < in.length; i++) {
+            for (int j = i; j < in.length; j++) {
+                if (in[j] < in[i]) {
+                    int temp = in[i];
+                    in[i] = in[j];
+                    in[j] = temp;
+                }
+            }
+        }
     }
 
     public static void init(){
@@ -55,31 +133,21 @@ public class Main {
         int y0 = minDist(y,x-1);
         int ym1 = minDist((((y-1)+map.length)%(map.length)),x-1);
 
-        if (yp1 <= y0 && yp1 <= ym1) {
-
-            costs[y][x] = yp1+map[y][x];
-            return costs[y][x];
-        }
         if (y0 <= yp1 && y0 <= ym1) {
 
             costs[y][x] = y0+map[y][x];
             return costs[y][x];
         }
-        if (ym1 <= y0 && ym1 <= yp1) {
+        if (yp1 <= y0 && yp1 <= ym1) {
 
-            costs[y][x] = ym1+map[y][x];
+            costs[y][x] = yp1+map[y][x];
             return costs[y][x];
         }
 
-        return -1; //TODO
-    }
 
-    public static void printArrs(){
-        for (int[] ints : map) {
-            System.out.println(Arrays.toString(ints));
-        }
-        for (int i = 0; i < map.length; i++) {
-            System.out.println(Arrays.toString(costs[i]));
-        }
+        costs[y][x] = ym1+map[y][x];
+        return costs[y][x];
+
+
     }
 }
